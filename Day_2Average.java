07a74f1ec11a4f93d7b877f0b5672f0bd1293429
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-24 16:10:45 +0530

    Create Day_2Average.java

---
+++ Day_2Average.java
@@ -0,0 +1,13 @@
+class Average{
+public static void main(String args[]){
+
+   	int   num1 = 10;
+	int   num2 = 12;
+	int   num3 = 14;
+	int   num4 = 16;
+	int   num5 = 18;
+
+		System.out.println("Average of five number is : "+(num1+num2+num3+num4+num5)/5);
+
+	}
+}
