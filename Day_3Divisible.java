810d53c1163135c4371886aad37d05e664d603b0
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-24 17:00:55 +0530

    Create Day_3Divisible.java

---
+++ Day_3Divisible.java
@@ -0,0 +1,19 @@
+class Divisible
+{
+              public static void main(String []args)
+                            {
+                             
+                                    int x = 21;
+                            if(x%3==0 || x%7==0)
+                                      {
+                                            
+                                             System.out.println("number is Divisible");
+                                        }
+                            else
+
+                                     {        
+                                             System.out.println("Number is not Divisible");
+                                      }
+                             }
+
+}
