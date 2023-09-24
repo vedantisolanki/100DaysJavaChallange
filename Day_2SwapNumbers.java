8e511cb637a6246a1444de58968cbadd0ef7e40e
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-24 16:29:02 +0530

    Create Day_2SwapNumbers.java

---
+++ Day_2SwapNumbers.java
@@ -0,0 +1,16 @@
+class SwapNumbers{
+public static void main(String[] args)
+	{
+
+	  int x = 25;
+	  int y = 50;
+	  int z;
+
+	  z = x;
+	  x = y;
+	  y = z;
+
+			System.out.println("x = "+x);
+			System.out.println("y = "+y);
+	}
+}
