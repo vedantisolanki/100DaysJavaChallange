4e5093131fe694f2f00a2492ac0cada7fcecb310
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-24 17:07:15 +0530

    Create Day_3NewLineEscapeCharacter.java

---
+++ Day_3NewLineEscapeCharacter.java
@@ -0,0 +1,7 @@
+class NewLineEscapeCharacter
+{
+	public static void main(String args[])
+  {
+	System.out.println(" Easy Hai Code \n Easy Hai Code \n Easy Hai Code \n Easy Hai Code \n Easy Hai 	Code");
+  }
+}
