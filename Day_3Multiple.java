041da815a4a1f3d02608f55637a1de7271e6ce63
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-24 17:12:07 +0530

    Create Day_3Multiple.java

---
+++ Day_3Multiple.java
@@ -0,0 +1,15 @@
+class Multiple
+{
+           public static void main(String []args)
+           {
+              int x=15;
+              if(x%5==0)
+                    {
+                         System.out.println(x+" is multiple of 5");
+                     }
+              else 
+                      {
+                         System.out.println(x+" is not multiple of 5");
+                     }
+            }
+}
