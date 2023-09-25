1f8de32db857d4e2325974fa2b6aa321186f03a4
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-25 18:51:02 +0530

    Create Day_4VoteForCountry.java

---
+++ Day_4VoteForCountry.java
@@ -0,0 +1,15 @@
+class VoteForCountry
+{
+public static void main(String[] args)
+{
+  int age = 22;
+  if(age>=18)
+  {
+  System.out.println("yes");
+  }
+  else
+  {
+  System.out.println("no");
+  }
+}
+}
