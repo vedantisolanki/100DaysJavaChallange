04a16fe1238e67c95db3ab4597990a7c4970b224
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-25 18:52:28 +0530

    Create Day_4CheckNumber.java

---
+++ Day_4CheckNumber.java
@@ -0,0 +1,15 @@
+class CheckNumber
+{
+public static void main(String[] args)
+{
+int num = 26;
+if(num>=21 && num<=575)
+{
+System.out.println("Yes");
+}
+else
+{
+System.out.println("No");
+}
+}
+}
