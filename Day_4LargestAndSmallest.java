806f37e058ee75e5f6da26571e905b37ea72c0b2
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-25 18:58:24 +0530

    Create Day_4LargestAndSmallest.java

---
+++ Day_4LargestAndSmallest.java
@@ -0,0 +1,32 @@
+
+class LargestAndSmallest
+{
+public static void main(String args[])
+  {
+
+	int a=25;
+	int b=20;
+	int c=2;
+
+if(a>b && a>c)
+	System.out.println(a +" :is largest number");
+	
+else if(b>a && b>c)
+	System.out.println(b +" :is largest number");
+
+else 
+	System.out.println(c +" :is largest number");
+
+
+if(a<b && a<c)
+	System.out.println(a +" :is Smallest number");
+
+else if(b<a && b<c)
+    	System.out.println(b +" :is smallest number");
+
+else 
+	System.out.println(c +" :is smallest number");
+
+
+ }
+}
