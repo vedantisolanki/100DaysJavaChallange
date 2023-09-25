84a0156a5fcc405123d826426632692d98972bd8
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-09-25 18:53:54 +0530

    Create Day_4Alphabet.java

---
+++ Day_4Alphabet.java
@@ -0,0 +1,15 @@
+class Alphabet
+{
+public static void main(String[] args)
+{
+char ch = 'a';
+if(ch>='a'&& ch<='z' || ch>='A'&& ch<='Z')
+{
+System.out.println("Alphabet");
+}
+else
+{
+System.out.println("Not Alphabet");
+}
+}
+}
