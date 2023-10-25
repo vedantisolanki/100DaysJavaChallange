f4e8fcaffc13e09edea5b5067910d85cd9e384de
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-10-25 14:35:26 +0530

    Create DAY_6LastTwoDigit.java

---
+++ DAY_6LastTwoDigit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class LastTwoDigit{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a num");
+int num=sc.nextInt();
+System.out.println("The last two digit of number is:"+num%100);
+}
+}
