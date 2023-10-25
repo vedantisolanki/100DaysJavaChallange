aa7e240f7c3e5af2b711be6e2373b17fd41caa37
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-10-25 14:07:56 +0530

    Create DAY_5LastDigitNumber.java

---
+++ DAY_5LastDigitNumber.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class LastDigit{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a num");
+int num=sc.nextInt();
+System.out.println("The last digit of number is:"+num%10);
+}
+}
