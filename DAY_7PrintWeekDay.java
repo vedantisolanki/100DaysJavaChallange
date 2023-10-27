7d68b9829b4245d5f8efa527a6c8f5bc20acf45c
vedantisolanki <145599313+vedantisolanki@users.noreply.github.com>
2023-10-27 21:42:49 +0530

    Create DAY_7PrintWeekDay.java

---
+++ DAY_7PrintWeekDay.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class PrintWeekDay{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a Week Number");
+int weeknumber= sc.nextInt();
+if(weeknumber==0){
+System.out.println("Sunday");
+}
+if(weeknumber==1){
+System.out.println("Monday");
+}
+if(weeknumber==2){
+System.out.println("Tuesday");
+}
+if(weeknumber==3){
+System.out.println("Wednesday");
+}
+if(weeknumber==4){
+System.out.println("Thursday");
+}
+if(weeknumber==5){
+System.out.println("Friday");
+}
+if(weeknumber==6){
+System.out.println("Saturday");
+}
+}
+}
+
